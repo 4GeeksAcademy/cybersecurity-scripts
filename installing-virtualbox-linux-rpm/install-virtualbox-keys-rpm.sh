529be7de12f696d60621fdcdd43d03fce7a06948
Arnaldo Perez <19396163+arnaldoperez@users.noreply.github.com>
2024-03-06 20:32:44 +0000

    Reorder scripts

--- installing-virtualbox-linux-rpm/install-virtualbox-keys-rpm.sh
+++ installing-virtualbox-linux-rpm/install-virtualbox-keys-rpm.sh
@@ -1,9 +1,2 @@
 
-os_codename=$(cat /etc/os-release | grep VERSION_CODENAME= | cut -d= -f2 | tr -d '"')
-
-echo "deb [arch=amd64 signed-by=/usr/share/keyrings/oracle-virtualbox-2016.gpg] https://download.virtualbox.org/virtualbox/debian $os_codename contrib"
-
-wget -O- https://www.virtualbox.org/download/oracle_vbox_2016.asc | sudo gpg --yes --output /usr/share/keyrings/oracle-virtualbox-2016.gpg --dearmor
-
-sudo apt-get update -y
-sudo apt-get install -y virtualbox
+wget -q https://www.virtualbox.org/download/oracle_vbox_2016.asc -O- | rpm --import -
