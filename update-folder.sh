5855b2e898a67befe9572b46361dc6088812139e
Arnaldo Perez <arnaldoeperezb@gmail.com>
2024-05-07 14:06:38 -0400

    scripts kali

--- update-folder.sh
+++ update-folder.sh
@@ -1,5 +1,22 @@
+#!/bin/bash
+
+# Cambiar al directorio del proyecto
 cd $HOME/cybersecurity-scripts
 
+# Actualizar la rama local
 git reset --hard origin/main
+git pull --allow-unrelated-histories origin main >>/home/deb/.sync-scripts.log 2> /home/deb/.sync-scripts.error.log
 
-git pull origin/main
+if [[ $? -ne 0 ]]; then
+  echo "Error: Script failed with exit code $?"
+  exit 1  # Exit with a non-zero code to signal failure
+fi
+
+fecha_hora=$(date +"%Y-%m-%d %H:%M:%S")
+
+# Agregar la marca temporal al script
+echo "$fecha_hora:"
+
+# Mostrar mensaje de éxito
+echo "Pull realizado correctamente para la rama main"
+echo "______________________________________________"
