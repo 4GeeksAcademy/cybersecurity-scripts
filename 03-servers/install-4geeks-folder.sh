c6a9755524912275ca27089c06b392426b6c03b9
Arnaldo Perez <arnaldoeperezb@gmail.com>
2024-05-07 11:24:52 -0400

    kali script

--- 03-servers/install-4geeks-folder.sh
+++ 03-servers/install-4geeks-folder.sh
@@ -1,3 +1,3 @@
-git clone  -C $HOME/cybersecurity-scripts https://github.com/4GeeksAcademy/cybersecurity-scripts.git
+git clone  -C ~/cybersecurity-scripts https://github.com/4GeeksAcademy/cybersecurity-scripts.git
 
 echo "@reboot bash $HOME/cybersecurity-scripts/update-folder.sh" | crontab -
